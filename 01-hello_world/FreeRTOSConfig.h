de9bf83dae70259873e03d2d524df718a31efb6e
Marcin Borowicz <marcinbor85@gmail.com>
2022-05-07 22:22:22 +0200

    fix tabs

--- 01-hello_world/FreeRTOSConfig.h
+++ 01-hello_world/FreeRTOSConfig.h
@@ -37,26 +37,26 @@
  * http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION					1
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION	0
-#define configUSE_IDLE_HOOK						0
-#define configUSE_TICK_HOOK						0
+#define configUSE_PREEMPTION				1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION		0
+#define configUSE_IDLE_HOOK				0
+#define configUSE_TICK_HOOK				0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK		0
-#define configTICK_RATE_HZ						( 1000 ) /* In this non-real time simulated environment the tick frequency has to be at least a multiple of the Win32 tick frequency, and therefore very slow. */
-#define configMINIMAL_STACK_SIZE				( ( unsigned short ) 70 ) /* In this simulated case, the stack only has to hold one small structure as the real stack is part of the win32 thread. */
-#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 65 * 1024 ) )
-#define configMAX_TASK_NAME_LEN					( 12 )
-#define configUSE_TRACE_FACILITY				0
-#define configUSE_16_BIT_TICKS					0
-#define configIDLE_SHOULD_YIELD					1
-#define configUSE_MUTEXES						1
+#define configTICK_RATE_HZ				( 1000 ) /* In this non-real time simulated environment the tick frequency has to be at least a multiple of the Win32 tick frequency, and therefore very slow. */
+#define configMINIMAL_STACK_SIZE			( ( unsigned short ) 70 ) /* In this simulated case, the stack only has to hold one small structure as the real stack is part of the win32 thread. */
+#define configTOTAL_HEAP_SIZE				( ( size_t ) ( 65 * 1024 ) )
+#define configMAX_TASK_NAME_LEN				( 12 )
+#define configUSE_TRACE_FACILITY			0
+#define configUSE_16_BIT_TICKS				0
+#define configIDLE_SHOULD_YIELD				1
+#define configUSE_MUTEXES				1
 #define configCHECK_FOR_STACK_OVERFLOW			0
-#define configUSE_RECURSIVE_MUTEXES				1
-#define configQUEUE_REGISTRY_SIZE				20
+#define configUSE_RECURSIVE_MUTEXES			1
+#define configQUEUE_REGISTRY_SIZE			20
 #define configUSE_APPLICATION_TASK_TAG			1
 #define configUSE_COUNTING_SEMAPHORES			1
-#define configUSE_ALTERNATIVE_API				0
-#define configUSE_QUEUE_SETS					1
+#define configUSE_ALTERNATIVE_API			0
+#define configUSE_QUEUE_SETS				1
 #define configUSE_TASK_NOTIFICATIONS			1
 #define configSUPPORT_STATIC_ALLOCATION			0
 
@@ -64,12 +64,12 @@
 priority is configMAX_PRIORITIES - 1.  The priority of the timer task is
 deliberately set higher to ensure it is correctly capped back to
 configMAX_PRIORITIES - 1. */
-#define configUSE_TIMERS						1
-#define configTIMER_TASK_PRIORITY				( configMAX_PRIORITIES - 1 )
-#define configTIMER_QUEUE_LENGTH				20
+#define configUSE_TIMERS				1
+#define configTIMER_TASK_PRIORITY			( configMAX_PRIORITIES - 1 )
+#define configTIMER_QUEUE_LENGTH			20
 #define configTIMER_TASK_STACK_DEPTH			( configMINIMAL_STACK_SIZE * 2 )
 
-#define configMAX_PRIORITIES					( 7 )
+#define configMAX_PRIORITIES				( 7 )
 
 /* Run time stats gathering configuration options. */
 unsigned long ulGetRunTimeCounterValue( void ); /* Prototype of function that returns run time counter. */
@@ -77,14 +77,14 @@ void vConfigureTimerForRunTimeStats( void );	/* Prototype of function that initi
 #define configGENERATE_RUN_TIME_STATS			0
 
 /* Co-routine related configuration options. */
-#define configUSE_CO_ROUTINES 					0
+#define configUSE_CO_ROUTINES 				0
 #define configMAX_CO_ROUTINE_PRIORITIES			( 2 )
 
 /* This demo can use of one or more example stats formatting functions.  These
 format the raw data provided by the uxTaskGetSystemState() function in to human
 readable ASCII form.  See the notes in the implementation of vTaskList() within
 FreeRTOS/Source/tasks.c for limitations. */
-#define configUSE_STATS_FORMATTING_FUNCTIONS	0
+#define configUSE_STATS_FORMATTING_FUNCTIONS		0
 
 /* Enables the test whereby a stack larger than the total heap size is
 requested. */
@@ -93,23 +93,23 @@ requested. */
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function.  In most cases the linker will remove unused
 functions anyway. */
-#define INCLUDE_vTaskPrioritySet				1
-#define INCLUDE_uxTaskPriorityGet				1
-#define INCLUDE_vTaskDelete						1
+#define INCLUDE_vTaskPrioritySet			1
+#define INCLUDE_uxTaskPriorityGet			1
+#define INCLUDE_vTaskDelete				1
 #define INCLUDE_vTaskCleanUpResources			0
-#define INCLUDE_vTaskSuspend					1
-#define INCLUDE_vTaskDelayUntil					1
-#define INCLUDE_vTaskDelay						1
+#define INCLUDE_vTaskSuspend				1
+#define INCLUDE_vTaskDelayUntil				1
+#define INCLUDE_vTaskDelay				1
 #define INCLUDE_uxTaskGetStackHighWaterMark		1
-#define INCLUDE_uxTaskGetStackHighWaterMark2	1
+#define INCLUDE_uxTaskGetStackHighWaterMark2		1
 #define INCLUDE_xTaskGetSchedulerState			1
-#define INCLUDE_xTimerGetTimerDaemonTaskHandle	1
+#define INCLUDE_xTimerGetTimerDaemonTaskHandle		1
 #define INCLUDE_xTaskGetIdleTaskHandle			1
-#define INCLUDE_xTaskGetHandle					1
-#define INCLUDE_eTaskGetState					1
+#define INCLUDE_xTaskGetHandle				1
+#define INCLUDE_eTaskGetState				1
 #define INCLUDE_xSemaphoreGetMutexHolder		1
 #define INCLUDE_xTimerPendFunctionCall			1
-#define INCLUDE_xTaskAbortDelay					1
+#define INCLUDE_xTaskAbortDelay				1
 
 #define configUSE_MALLOC_FAILED_HOOK			0
 
