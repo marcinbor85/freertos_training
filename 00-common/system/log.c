40e3a45ada2669a17a98797e3c3755c70d306622
Marcin Borowicz <marcinbor85@gmail.com>
2022-05-14 17:59:45 +0200

    improve log format

--- 00-common/system/log.c
+++ 00-common/system/log.c
@@ -37,6 +37,7 @@ void log_printf(int level, const char *file, const char *func, unsigned long lin
 {
         char level_name[8] = {0};
         char prefix[8] = {0};
+        static uint32_t cntr = 0;
 
         system_stdout_lock();
 
@@ -55,7 +56,8 @@ void log_printf(int level, const char *file, const char *func, unsigned long lin
         }
         
         TickType_t ticks = xTaskGetTickCount();
-        printf("%s%08lu %s %s@%s:%lu ", prefix, ticks, level_name, func, file, line);
+        printf("%s%04u %08lu %s %s %s:%lu > ", prefix, cntr, ticks, level_name, func, file, line);
+        ++cntr;
 
         va_list arg;
         va_start( arg, fmt );
