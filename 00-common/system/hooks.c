2e8039e268e4c103f05f71abce6e4b2b76e17ff7
Marcin Borowicz <marcinbor85@gmail.com>
2022-05-22 12:43:32 +0200

    fix vApplicationStackOverflowHook once again

--- 00-common/system/hooks.c
+++ 00-common/system/hooks.c
@@ -3,8 +3,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#include "system/log.h"
-
 void vApplicationMallocFailedHook(void)
 {
         configASSERT(pdFALSE);
@@ -13,9 +11,9 @@ void vApplicationMallocFailedHook(void)
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
 {
-        if (pcTaskName == NULL)
-                pcTaskName = "NULL";
-        LOG_F("task: <%s>", pcTaskName);
+        (void)xTask;
+        (void)pcTaskName;
+        
         configASSERT(pdFALSE);
         exit(-1);
 }
